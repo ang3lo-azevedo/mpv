30c1da1f4916d8e18349e59e933d35953d2ee710
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-06-11 01:14:19 +0100

    refactor: remove legacy mpv scripts and unused VapourSynth configurations

--- list_scripts.lua
+++
@@ -1,6 +0,0 @@
-local mp = require 'mp'
-mp.register_event("start-file", function()
-    for _, s in ipairs(mp.get_property_native("script-list") or {}) do
-        print("LOADED SCRIPT: " .. s.name .. " from " .. (s.filename or "unknown"))
-    end
-end)
--- load_dir_test.lua
+++
@@ -1,8 +0,0 @@
-local mp = require 'mp'
-mp.register_event("file-loaded", function()
-    local dir = mp.command_native({"expand-path", "~~/scripts/ui/other/thumbnails/thumbfast"})
-    mp.commandv("load-script", dir)
-    for _, s in ipairs(mp.get_property_native("script-list") or {}) do
-        print("LOADED SCRIPT: " .. s.name .. " from " .. (s.filename or "unknown"))
-    end
-end)
