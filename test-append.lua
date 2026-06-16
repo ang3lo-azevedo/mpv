29e5429b6806eea8af76a250aa50cf2c3a3a6260
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-06-16 19:09:50 +0100

    refactor: remove temporary debug logs, test scripts, and patch management utilities

--- test-append.lua
+++
@@ -1,8 +0,0 @@
-local mp = require 'mp'
-mp.register_event("file-loaded", function()
-    local dir = mp.command_native({"expand-path", "~~/scripts/ui/other/thumbnails/thumbfast"})
-    mp.set_property("scripts-append", dir)
-    for _, s in ipairs(mp.get_property_native("script-list") or {}) do
-        print("LOADED SCRIPT: " .. s.name .. " from " .. (s.filename or "unknown"))
-    end
-end)
--- test_env.lua
+++
@@ -1,4 +0,0 @@
-mp.register_event("file-loaded", function()
-    local res = mp.command_native_async({name = "subprocess", args = {"ls"}, env = "PATH=/usr/bin"}, function() print("Done") end)
-    print("res: " .. tostring(res))
-end)
